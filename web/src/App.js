12a0bfefee5db046fe7cca478d50b90f7071ff1e
Matheus Marques <Marques537@users.noreply.github.com>
2020-01-19 03:28:46 -0300

    create frontend

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,50 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+import api from './services/api';
+import DevItem from './components/DevItem/index.js';
+import DevForm from './components/DevForm/Index.js';
 // Componente: bloco isolado de HTML, CSS  e JS. o qual não interfere no restante da aplicação
 // Propriedade: Informações que um componente PAI passa para o componente FILHO
 // Estado: Informações mantidas pelo compomente (lembar: imutabilidade)
 
-
-
 function App() {
-  const [counter, setCounter] = useState(0);
-  
-  function incrementCounter(){
-    setCounter (counter + 1);
+  const [devs, setDevs] = useState ([ ]);
 
-  }
+  useEffect(()=> {
+    async function loadDevs(){
+      const response = await api.get('/devs');
 
-  return (
-    <>
-<h1> Contador: {counter}</h1>
-    <button onClick={incrementCounter}> Incrementar</button>
-    </>
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+async function handleAddDev(data) {
+
+  const response = await api.post('/devs',data)
+
+  setDevs([...devs, response.data]);
+}
+  return(
+  <div id="app"> 
+    <aside>
+      <strong>Cadastrar</strong>
+      <DevForm onSubmit={handleAddDev}/>
+    </aside>
+
+    <main> 
+      <ul>
+        {devs.map(dev =>(
+          <DevItem key={dev._id} dev={dev }/>
+          ))}
+      </ul>
+    </main> 
+  </div>
   );
 }
 
